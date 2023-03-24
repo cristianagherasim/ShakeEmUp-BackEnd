286438db0b6249a055b3c3b5a9e8fdc0a05f6f7a
jsenyko <jakobsenyko@gmail.com>
2023-03-23 22:09:25 -0400

    pushing

--- functions/src/routes/DrinkRoutes.tsx
+++ functions/src/routes/DrinkRoutes.tsx
@@ -1,6 +1,6 @@
 import express, {Request, Response} from "express";
 import {getClient} from "../routes/db";
-//import {ObjectId} from "../routes/db";
+import {ObjectId} from "mongodb";
 
 import DrinkModel from "../models/DrinkModel";
 
@@ -22,5 +22,71 @@ shoutOutRoutes.get("/", async (req:Request, res:Response) => {
       return res.status(200).json(results);
     } catch (error) {
       return res.status(500).send(error);
+    }
+  });
+
+  shoutOutRoutes.get("/", async (req:Request, res:Response) => {
+    const id = req.params.id;
+    try {
+      const client = await getClient();
+      const result = await client.db().collection<DrinkModel>("drink").findOne({_id: new ObjectId(id)});
+  
+      if (!result) {
+        return res.status(404).send("Drink not found");
+      }
+  
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  });
+
+  shoutOutRoutes.post("/", async (req:Request, res:Response) => {
+    const shoutOut = req.body as DrinkModel;
+  
+    try {
+      const client = await getClient();
+  
+      await client.db().collection<DrinkModel>("drinks").insertOne(shoutOut);
+  
+      return res.status(201).json(shoutOut);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  });
+
+  shoutOutRoutes.put("/", async (req:Request, res:Response) => {
+    const id = req.params.id;
+    const cockTail = req.body as DrinkModel;
+   
+  
+    try {
+      const client = await getClient();
+      const result = await client.db().collection<DrinkModel>("drinks").replaceOne({strDrink: new String(id)}, cockTail);
+  
+      if (result.modifiedCount === 0) {
+        return res.status(404).send("Not found");
+      } else {
+        //cockTail.strDrink = new String(id);
+        return res.json(cockTail);
+      }
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  });
+
+  shoutOutRoutes.delete("/", async (req:Request, res:Response) => {
+    const id = req.params.id;
+    try {
+      const client = await getClient();
+      const result = await client.db().collection<DrinkModel>("drinks").deleteOne({strDrink: new String(id)});
+      if (result.deletedCount === 0) {
+        return res.status(404).json({message: "Not Found"});
+      } else {
+        return res.status(204).end();
+      }
+    } catch (err) {
+      console.error("FAIL", err);
+      return res.status(500).json({message: "Internal Server Error"});
     }
   });
