276e33d9fc263414a56e11ee1cfc4373c95b6625
jsenyko <jakobsenyko@gmail.com>
2023-04-01 20:57:58 -0400

    pushIt

--- functions/src/models/DrinkModel.ts
+++ functions/src/models/DrinkModel.ts
@@ -11,7 +11,8 @@ export interface Root {
    drinks: Drink[]
  }
  export interface Drink {
-   idDrink: string
+  
+  idDrink: string
    strDrink: string
    strDrinkAlternate: any
    strTags: any
--- functions/src/routes/UserSubmittedDrinkRoutes.ts
+++ functions/src/routes/UserSubmittedDrinkRoutes.ts
@@ -1,8 +1,9 @@
 import express, {Request, Response} from "express";
 import {getClient} from "./db";
 import {ObjectId} from "mongodb";
+import { Drink } from "../models/DrinkModel";
 
-import {UserDrink} from "../models/UserDrinkModel";
+
 
 export const cocktailRoutes = express.Router();
 
@@ -11,7 +12,7 @@ cocktailRoutes.get("/", async (req:Request, res:Response) => {
   
     try {
       const client = await getClient();
-      const results = await client.db("UserSubmittedDrinks").collection<UserDrink>("userDrinks").find({}).toArray();
+      const results = await client.db("UserSubmittedDrinks").collection<Drink>("userDrinks").find({}).toArray();
   
       return res.status(200).json(results);
     } catch (error) {
@@ -23,7 +24,7 @@ cocktailRoutes.get("/", async (req:Request, res:Response) => {
     const id = req.params.id;
     try {
       const client = await getClient();
-      const result = await client.db("UserSubmittedDrinks").collection<UserDrink>("userDrinks").findOne({_id: new ObjectId(id)});
+      const result = await client.db("UserSubmittedDrinks").collection<Drink>("userDrinks").findOne({_id: new ObjectId(id)});
   
       if (!result) {
         return res.status(404).send("Drink not found");
@@ -39,19 +40,80 @@ cocktailRoutes.get("/", async (req:Request, res:Response) => {
     const id = req.params.id;
    
     const newDrink = {
+      _id: req.body._id,
       newDrinkId: id,
+      // strDrink: req.body.strDrink,
+      // strGlass: req.body.strGlass,
+      // strIngredient1: req.body.strIngredient1,
+      // strMeasure1: req.body.strMeasure,
+      // strInstructions: req.body.strInstructions,
+      // strImageSource: req.body.strImageSource,
+      idDrink: req.body.idDrink,
       strDrink: req.body.strDrink,
+      strDrinkAlternate: req.body.strDrinkAlternate,
+      strTags: req.body.strTags,
+      strVideo: req.body.strVideo,
+      strCategory: req.body.strCategory,
+      strIBA: req.body.strIBA,
+      strAlcoholic: req.body.strAlcoholic,
       strGlass: req.body.strGlass,
-      strIngredient1: req.body.strIngredient,
-      strMeasure1: req.body.strMeasure,
       strInstructions: req.body.strInstructions,
+      strInstructionsES: req.body.strInstructionsES,
+      strInstructionsDE: req.body.strInstructionsDE,
+      strInstructionsFR: req.body.strInstructionsFR,
+      strInstructionsIT: req.body.strInstructionsIT,
+     // "strInstructionsZH-HANS": any
+     // "strInstructionsZH-HANT": any
+      strDrinkThumb: req.body.strDrinkThumb,
+      strIngredient1: req.body.strIngredient1,
+      strIngredient2: req.body.strIngredient2,
+      strIngredient3: req.body.strIngredient3,
+      strIngredient4: req.body.strIngredient4,
+      strIngredient5: req.body.strIngredient5,
+      strIngredient6: req.body.strIngredient6,
+      strIngredient7: req.body.strIngredient7,
+      strIngredient8: req.body.strIngredient8,
+      strIngredient9: req.body.strIngredient9,
+      strIngredient10: req.body.strIngredient10,
+      strIngredient11: req.body.strIngredient11,
+      strIngredient12: req.body.strIngredient12,
+      strIngredient13: req.body.strIngredient13,
+      strIngredient14: req.body.strIngredient14,
+      strIngredient15: req.body.strIngredient15,
+      strMeasure1: req.body.strMeasure1,
+      strMeasure2: req.body.strMeasure2,
+      strMeasure3: req.body.strMeasure3,
+      strMeasure4: req.body.strMeasure4,
+      strMeasure5: req.body.strMeasure5,
+      strMeasure6: req.body.strMeasure6,
+      strMeasure7: req.body.strMeasure7,
+      strMeasure8: req.body.strMeasure8,
+      strMeasure9: req.body.strMeasure9,
+      strMeasure10: req.body.strMeasure10,
+      strMeasure11: req.body.strMeasure11,
+      strMeasure12: req.body.strMeasure12,
+      strMeasure13: req.body.strMeasure13,
+      strMeasure14: req.body.strMeasure14,
+      strMeasure15: req.body.strMeasure15,
       strImageSource: req.body.strImageSource,
-    } as unknown as UserDrink;
+      strImageAttribution: req.body.strImageAttribution,
+      strCreativeCommonsConfirmed: req.body.strCreativeCommonsConfirmed,
+      dateModified: req.body.dateModified
+
+
+
+
+
+
+
+
+
+    } as unknown as Drink;
   
     try {
       const client = await getClient();
   
-      await client.db("UserSubmittedDrinks").collection<UserDrink>("userDrinks").insertOne(newDrink);
+      await client.db("UserSubmittedDrinks").collection<Drink>("userDrinks").insertOne(newDrink);
   
       return res.status(201).json(newDrink);
     } catch (error) {
@@ -62,12 +124,12 @@ cocktailRoutes.get("/", async (req:Request, res:Response) => {
   //put is update something that already exists
   cocktailRoutes.put("/:id", async (req:Request, res:Response) => {
     const id = req.params.id;
-    const cockTail = req.body as UserDrink;
+    const cockTail = req.body as Drink;
    
   
     try {
       const client = await getClient();
-      const result = await client.db("UserSubmittedDrinks").collection<UserDrink>("userDrinks").replaceOne({_id: new ObjectId(id)}, cockTail);
+      const result = await client.db("UserSubmittedDrinks").collection<Drink>("userDrinks").replaceOne({_id: new ObjectId(id)}, cockTail);
   
       if (result.modifiedCount === 0) {
         return res.status(404).send("Not found");
@@ -84,7 +146,7 @@ cocktailRoutes.get("/", async (req:Request, res:Response) => {
     const id = req.params.id;
     try {
       const client = await getClient();
-      const result = await client.db("UserSubmittedDrinks").collection<UserDrink>("userDrinks").deleteOne({_id: new ObjectId(id)});
+      const result = await client.db("UserSubmittedDrinks").collection<Drink>("userDrinks").deleteOne({_id: new ObjectId(id)});
       if (result.deletedCount === 0) {
         return res.status(404).json({message: "Not Found"});
       } else {
